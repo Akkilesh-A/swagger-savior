ae77901e44e7c9cbaeac9a94640c7656b3f0d614
Akkilesh-A <akkilalagar05@gmail.com>
2024-12-30 15:20:32 +0530

    feat: add custom token name input field and ui improvements

--- src/App.tsx
+++ src/App.tsx
@@ -9,27 +9,7 @@ export default function App() {
   const [inputJSON,setInputJSON] = useState("")
   const [protocol,setProtocol]=useState("https")
   const [backendURL, setbackendURL] = useState("")
-
-  async function autoSelectProtocol() {
-    const [tab]=await chrome.tabs.query({active:true})
-    chrome.scripting.executeScript({
-      target:{tabId:tab.id!},
-      func:async (protocol) => {
-        //Schemes and Authorize button bar
-        const ui=document.body.firstElementChild?.firstElementChild?.getElementsByClassName("swagger-ui")[0].lastElementChild?.getElementsByClassName("scheme-container")[0]
-        //Select https in dropdown menu
-        const https=ui?.firstElementChild?.firstElementChild?.lastElementChild as HTMLSelectElement
-        console.log("before val",https.value)
-
-        //Select option with specified protocol
-        const protocolOption=https.querySelector(`option[value=${protocol}]`) as HTMLOptionElement
-        protocolOption.selected=true
-        console.log("after val",https.value)
-        console.log("finished")
-      },
-      args:[protocol]
-    })
-  }
+  const [tokenName,setTokenName] = useState("")
 
   useEffect(()=>{
     const savedJSON=localStorage.getItem("swagger-auth-help-JSON")
@@ -45,16 +25,31 @@ export default function App() {
     if(savedProtocol){
       setProtocol(savedProtocol!)
     }
-    autoSelectProtocol()
+    const savedTokenName=localStorage.getItem("swagger-auth-help-Token-Name")
+    if(savedTokenName){
+      const savedTokenNameVal=savedTokenName.split('"')[1]
+      setTokenName(savedTokenNameVal!)
+    }
   },[])
 
-  async function onclick(backendURL:string,jsonValue:string){
+  async function onclick(protocol:string,backendURL:string,jsonValue:string,tokenName:string){
     const [tab]=await chrome.tabs.query({active:true})
     chrome.scripting.executeScript({
       target:{tabId:tab.id!},
-      func:async (backendURL,jsonValue) => {
+      func:async (protocol,backendURL,jsonValue,tokenName) => {
+        async function autoSelectProtocol(){
+          //Schemes and Authorize button bar
+          const ui=document.body.firstElementChild?.firstElementChild?.getElementsByClassName("swagger-ui")[0].lastElementChild?.getElementsByClassName("scheme-container")[0]
+          //Select https in dropdown menu
+          const httpsSelectMenu=ui?.firstElementChild?.firstElementChild?.lastElementChild as HTMLSelectElement
+          httpsSelectMenu.click()
+          console.log(protocol)
+          // //Select option with specified protocol
+          // const protocolOption=https.querySelector(`option[value=${protocol}]`) as HTMLOptionElement
+          // console.log(protocolOption)
+        }
+        
         const backendLink=backendURL
-        console.log(backendLink)
         const response=await fetch(backendLink,{
           headers: {
             "Content-Type": "application/json",
@@ -63,8 +58,7 @@ export default function App() {
           body:jsonValue
         })
         const result=await response.json()
-        console.log(result.data.accessToken)
-        const token=result.data.accessToken
+        const token=result.data[tokenName]
 
         //Schemes and Authorize button bar
         const ui=document.body.firstElementChild?.firstElementChild?.getElementsByClassName("swagger-ui")[0].lastElementChild?.getElementsByClassName("scheme-container")[0]
@@ -75,25 +69,21 @@ export default function App() {
 
         //Get the form "model-ux-content"
         const form=ui?.firstElementChild?.lastElementChild?.lastElementChild?.lastElementChild?.firstElementChild?.firstElementChild?.lastElementChild?.lastElementChild?.firstElementChild as HTMLFormElement
-        console.log(form)
 
         //Get Input box
         const searchBox=form?.firstElementChild?.firstElementChild?.lastElementChild?.lastElementChild?.firstElementChild as HTMLInputElement
         searchBox.value="Bearer "+token
-        console.log(searchBox.value)
         
-        //Click on auhtorize button
+        //Click on authorize button
         const authorize=form?.lastElementChild?.firstElementChild as HTMLButtonElement
-        console.log(authorize)
 
-          // Trigger the 'input' event
+        // Trigger the 'input' event
         const inputEvent = new Event('input', { bubbles: true });
         searchBox.dispatchEvent(inputEvent);
 
         // Trigger the 'change' event
         const changeEvent = new Event('change', { bubbles: true });
         searchBox.dispatchEvent(changeEvent);
-        // authorize.click()
 
         if (authorize) {
           setTimeout(() => {
@@ -104,29 +94,28 @@ export default function App() {
         
             // Simulate a user click
             authorize.click();
-            console.log("Authorize button clicked");
 
             //Close the modal
             const modalClose=ui?.firstElementChild?.lastElementChild?.lastElementChild?.lastElementChild?.firstElementChild?.firstElementChild?.firstElementChild?.lastElementChild as HTMLElement
             modalClose.click()
-            console.log(modalClose)
+            autoSelectProtocol()
           }, 500);
         }
-
         console.log("finished")
       },
-      args:[backendURL,jsonValue]
+      args:[protocol,backendURL,jsonValue,tokenName]
     })
   }
 
   async function saveJSONToLocalStorage(){
     await localStorage.setItem("swagger-auth-help-JSON",JSON.stringify(inputJSON))
     await localStorage.setItem("swagger-auth-help-BackendURL",JSON.stringify(backendURL))
+    await localStorage.setItem("swagger-auth-help-Token-Name",JSON.stringify(tokenName))
   }
 
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div className="p-2 w-[300px] flex space-y-4 flex-col font-mono">
+      <div className="p-2 w-[400px] flex space-y-4 flex-col font-mono">
         <nav className="flex justify-between items-center">
           <img src="/images/icon-32.png" alt="Swagger_Auth_Help icon" />
           <p className="font-extrabold text-lg">Swagger Savior ✌️</p>
@@ -147,30 +136,45 @@ export default function App() {
         </div>
 
         <div className="space-y-2">
-          <div className="flex justify-between items-center">
-          <Label htmlFor="backendURL">Backend URL</Label>  
-            <Trash2 className="cursor-pointer" onClick={()=>{
+          <Label htmlFor="token">Token Name</Label>    
+          <div className="flex space-x-2 items-center">
+            <Input defaultValue={tokenName} placeholder="Token name in JSON" onChange={(e)=>setTokenName(e.target.value)} />
+            <Button type="button" onClick={()=>{
+                localStorage.removeItem("swagger-auth-help-Token-Name")
+                setTokenName("")
+                }} variant="destructive" size="icon">
+              <Trash2 />
+            </Button>
+          </div>
+        </div>
+
+        <div className="space-y-2">
+          <Label htmlFor="backendURL">Login Endpoint URL</Label>  
+          <div className="flex space-x-2 items-center">
+            <Input defaultValue={backendURL} onChange={(ev)=>setbackendURL(ev.target.value)}/>
+            <Button  onClick={()=>{
                 localStorage.removeItem("swagger-auth-help-BackendURL")
                 setbackendURL("")
-                }} />
+              }} variant="destructive" size="icon" type="button">
+            <Trash2 />
+            </Button>
           </div>
-          <Input defaultValue={backendURL} onChange={(ev)=>setbackendURL(ev.target.value)}/>
         </div>
         
         <div className="space-y-2">
           <div className="flex justify-between items-center">
             <Label htmlFor="inputJSON">Input JSON</Label>  
-            <Trash2 className="cursor-pointer" onClick={()=>{
+            <Button variant="destructive" onClick={()=>{
                 localStorage.removeItem("swagger-auth-help-JSON")
                 setInputJSON("")
-                }} />
+                }}>Delete JSON</Button>
           </div>
           <Textarea defaultValue={inputJSON} rows={6} onChange={(ev)=>setInputJSON(ev.target.value)}/>
         </div>
 
         <div className="flex items-center justify-around">
           <Button onClick={()=>saveJSONToLocalStorage()}>Save</Button>
-          <Button onClick={()=>onclick(backendURL,inputJSON)}>Login</Button>
+          <Button onClick={()=>onclick(protocol,backendURL,inputJSON,tokenName)}>Login</Button>
         </div>
       </div>
     </ThemeProvider>
